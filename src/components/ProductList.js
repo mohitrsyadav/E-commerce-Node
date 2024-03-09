873ea4bd433e22a2d4f352d6e979e4f9d4b9a8ba
mohitrsyadav <mohitrsyadav@gmail.com>
2024-03-09 12:51:37 +0530

    Update product API implimentation

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import {Link} from "react-router-dom";
 const ProductList = () => {
    const [product, setProduct] = useState([]); 
 
@@ -11,6 +12,16 @@ const ProductList = () => {
     result = await result.json();
     setProduct(result);
    }
+
+   const deleteProduct = async (id)=>{
+      let result = await fetch(`http://localhost:5000/product/${id}`, {
+         method:"Delete"
+      });
+      result = await result.json();
+      if(result){
+         alert("Record is deleted");
+      }
+   }
    
     return (
         <div className="product-list">    
@@ -21,15 +32,22 @@ const ProductList = () => {
             <li>Price</li>
             <li>Category</li>
             <li>Company</li>
+            <li>Operation</li>
          </ul>
 
-         {product && product.map(({name, price, category, company})=>(
-            <ul>
+         {product && product.map(({name, price, category, company, _id})=>(
+            <ul key ={_id}>
             <li>S.No</li>
             <li>{name}</li>
             <li>{price}</li>
             <li>{category}</li>
             <li>{company}</li>
+            <li>
+            <button onClick={()=>deleteProduct(_id)}>button</button>
+            <Link to={`/update/${_id}`}>Update</Link>
+            </li>
+           
+            
          </ul>
          ))}
         </div>
